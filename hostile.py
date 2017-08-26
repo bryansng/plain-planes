fab8e20b219396719720219b105c9182525a1268
letsdie10 <sngby98@gmail.com>
2017-08-27 05:11:17 +0800

    Added Advanced Helicopter

--- hostile.py
+++ hostile.py
@@ -74,4 +74,41 @@ class Rocket(Sprite):
 		# Only for individual bullets, sprites are prefered to use 
 		# self.draw(self.screen) or rockets.draw(screen)
 		self.screen.blit(self.image, self.rect)
+		
+class Advanced_Helicopter(Sprite):
+	"""A class to represent one of the hostile objects, Advanced Helicopter."""
+	def __init__(self, ai_settings, screen):
+		"""Initialize Advanced Helicopter settings."""
+		super().__init__()
+		self.ai_settings = ai_settings
+		self.screen = screen
+		
+		# Loads the image, get image rect, get screen rect.
+		self.image = pygame.image.load('images/objects/hostiles/advanced_attack_helicopter.bmp')
+		self.rect = self.image.get_rect()
+		self.screen_rect = self.screen.get_rect()
+		
+		# Positions the helicopter to the bottom right corner of the screen.
+		# It can't be seen in the screen.
+		self.rect.x = self.screen_rect.width
+		self.rect.y = self.screen_rect.height
+		
+		# Float values to a new variable if not decimals will not be registered.
+		self.centerx = float(self.rect.centerx)
+		self.centery = float(self.rect.centery)
+		
+	def update(self):
+		"""Updates the position of advanced helicopter."""
+		# Rockets will move from the bottom to the top, so just minus y like so.
+		self.centerx -= self.ai_settings.ad_heli_speed_factor
+		
+		# Float values are converted to integers and assigned back to the rect.
+		self.rect.centerx = self.centerx
+		self.rect.centery = self.centery
+		
+	def blitme(self):
+		"""Draws the Advanced Helicopter onto the screen."""
+		# Only for individual bullets, sprites are prefered to use 
+		# self.draw(self.screen) or adhelis.draw(screen)
+		self.screen.blit(self.image, self.rect)
 
