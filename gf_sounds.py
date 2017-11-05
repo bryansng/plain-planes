96b3813ffafe33b2b07d2519d1a0ff5b3c05b255
letsdie10 <sngby98@gmail.com>
2017-11-05 16:03:58 +0000

    Added comments

--- gf_sounds.py
+++ gf_sounds.py
@@ -2,22 +2,55 @@ import pygame
 
 from gf_universals import get_process_time
 
-def update_sounds(ai_settings, shipbullet_sounds, time_new):
+def update_sounds_internals(ai_settings, shipbullet_sounds, time_new):
+	"""
+	Upon calling this method, it will update/handle all game sounds that
+	will not stop unless a certain conditioned is reached.
+	
+	1. Shipbullet
+		Keeps playing firing mode sound until player stops firing.
+	"""
 	# Calls the function shipbullet_sound_firing_internals.
-	shipbullet_sound_firing_internals(ai_settings, shipbullet_sounds, time_new)
+	shipbullet_sound_firing_internals(ai_settings, shipbullet_sounds, time_new)	
+
+
+
+
+"""_____________________________________________________________________________
+   1a) ShipBullet Sound Internals: 
+       Start + Firing + End
+_____________________________________________________________________________"""
 		
 def shipbullet_sound_start_internals(ai_settings, shipbullet_sounds):
+	"""
+	Upon calling this method, it will play the shipbullet firing start sound
+	and it will get the stop time for the start sound.
+	"""
+	# Starts playing the start sound.
 	shipbullet_sounds.start.play()
 	
+	# Gets the stop time for start sound, so that we know when it should stop.
+	#
+	# If we hold left mouse button or spacebar, this will just run once.
+	# The below is used to update the shipbullet_sound_firing_internals.
 	ai_settings.shipbullet_sound_start_stop = ai_settings.shipbullet_time_fire + shipbullet_sounds.start.get_length()
 	
 def shipbullet_sound_firing_internals(ai_settings, shipbullet_sounds, time_new):
+	"""
+	Requires consistent update, hence placement in update_sounds_internals.
+	
+	Keeps playing the firing sound after the start sound ended.
+	"""
+	# If shipbullet firing start sound stopped/ended/finished and player is
+	# still firing bullets, then play the shipbullet firing fire sound.
 	if time_new >= ai_settings.shipbullet_sound_start_stop and ai_settings.shipbullets_constant_firing:
-		print("time_new", time_new)
-		print("shipbullet_sound_start_stop", ai_settings.shipbullet_sound_start_stop)
 		shipbullet_sounds.firing.play()
 	
 def shipbullet_sound_end_internals(ai_settings, shipbullet_sounds):
+	"""
+	Upon calling this method, it will stop the shipbullet firing fire sound
+	and it will play the shipbullet firing end sound.
+	"""
 	shipbullet_sounds.firing.stop()
 	shipbullet_sounds.end.play()
 	
