c649e44c04b7c81b7202545405987c6f9db4a440
letsdie10 <sngby98@gmail.com>
2018-01-04 14:27:17 +0000

    Updated Comments

--- upgrades.py
+++ upgrades.py
@@ -2,98 +2,146 @@ import pygame
 from pygame.sprite import Sprite
 
 class UpgradeRailguns(Sprite):
-	def __init__(self, ai_settings, screen, parachutes):
+	"""Represents the UpgradeRailguns drop that can be shot down."""
+	def __init__(self, ai_settings, screen):
+		"""Initializes the UpgradeRailguns settings."""
 		super().__init__()
 		self.ai_settings = ai_settings
 		self.screen = screen
 		
+		# Load image and get image rect.
 		self.image = pygame.image.load("images/upgrades/railguns.bmp")
 		self.rect = self.image.get_rect()
 		
+		# Position the upgrade image anywhere first.
 		self.rect.x = 0
 		self.rect.y = 0
 		
+		# Float values to a new variable if not decimals will not be registered.
 		self.centerx = float(self.rect.centerx)
 		self.centery = float(self.rect.centery)
 		
 	def update(self):
+		"""Updates the position of the Upgrade."""
+		# UpgradeRailguns will be dropped and move to the left of the screen,
+		# towards the ship.
 		self.centerx -= self.ai_settings.upgrades_speed_factor
 		
+		# Float values are converted to integers and assigned back to the rect.
 		self.rect.centerx = self.centerx
 		self.rect.centery = self.centery
 		
 	def blitme(self):
+		"""Draws the Upgrade onto the screen."""
+		# Only for individual upgrades, sprites are preferred to use
+		# self.draw(self.screen) or u_rails.draw(screen)
 		self.screen.blit(self.image, self.rect)
 	
 class UpgradeSecondaryGun(Sprite):
-	def __init__(self, ai_settings, screen, parachutes):
+	"""Represents the UpgradeSecondaryGun drop that can be shot down."""
+	def __init__(self, ai_settings, screen):
+		"""Initializes the UpgradeSecondaryGun settings."""
 		super().__init__()
 		self.ai_settings = ai_settings
 		self.screen = screen
 		
+		# Load image and get image rect.
 		self.image = pygame.image.load("images/upgrades/secondary.bmp")
 		self.rect = self.image.get_rect()
 		
+		# Position the upgrade image anywhere first.
 		self.rect.x = 0
 		self.rect.y = 0
 		
+		# Float values to a new variable if not decimals will not be registered.
 		self.centerx = float(self.rect.centerx)
 		self.centery = float(self.rect.centery)
 		
 	def update(self):
+		"""Updates the position of the Upgrade."""
+		# UpgradeSecondaryGun will be dropped and move to the left of the screen,
+		# towards the ship.
 		self.centerx -= self.ai_settings.upgrades_speed_factor
 		
+		# Float values are converted to integers and assigned back to the rect.
 		self.rect.centerx = self.centerx
 		self.rect.centery = self.centery
 		
 	def blitme(self):
+		"""Draws the Upgrade onto the screen."""
+		# Only for individual upgrades, sprites are preferred to use
+		# self.draw(self.screen) or u_secondary.draw(screen)
 		self.screen.blit(self.image, self.rect)
 	
 class UpgradeMissiles(Sprite):
-	def __init__(self, ai_settings, screen, parachutes):
+	"""Represents the UpgradeMissiles drop that can be shot down."""
+	def __init__(self, ai_settings, screen):
+		"""Initializes the UpgradeMissiles settings."""
 		super().__init__()
 		self.ai_settings = ai_settings
 		self.screen = screen
 		
+		# Load image and get image rect.
 		self.image = pygame.image.load("images/upgrades/missile.bmp")
 		self.rect = self.image.get_rect()
 		
+		# Position the upgrade image anywhere first.
 		self.rect.x = 0
 		self.rect.y = 0
 		
+		# Float values to a new variable if not decimals will not be registered.
 		self.centerx = float(self.rect.centerx)
 		self.centery = float(self.rect.centery)
 		
 	def update(self):
+		"""Updates the position of the Upgrade."""
+		# UpgradeMissiles will be dropped and move to the left of the screen,
+		# towards the ship.
 		self.centerx -= self.ai_settings.upgrades_speed_factor
 		
+		# Float values are converted to integers and assigned back to the rect.
 		self.rect.centerx = self.centerx
 		self.rect.centery = self.centery
 		
 	def blitme(self):
+		"""Draws the Upgrade onto the screen."""
+		# Only for individual upgrades, sprites are preferred to use
+		# self.draw(self.screen) or u_missile.draw(screen)
 		self.screen.blit(self.image, self.rect)
 	
 class UpgradeLaser(Sprite):
-	def __init__(self, ai_settings, screen, parachutes):
+	"""Represents the UpgradeLaser drop that can be shot down."""
+	def __init__(self, ai_settings, screen):
+		"""Initializes the UpgradeLaser settings."""
 		super().__init__()
 		self.ai_settings = ai_settings
 		self.screen = screen
 		
+		# Load image and get image rect.
 		self.image = pygame.image.load("images/upgrades/laser.bmp")
 		self.rect = self.image.get_rect()
 		
+		# Position the upgrade image anywhere first.
 		self.rect.x = 0
 		self.rect.y = 0
 		
+		# Float values to a new variable if not decimals will not be registered.
 		self.centerx = float(self.rect.centerx)
 		self.centery = float(self.rect.centery)
 		
 	def update(self):
+		"""Updates the position of the Upgrade."""
+		# UpgradeLaser will be dropped and move to the left of the screen,
+		# towards the ship.
 		self.centerx -= self.ai_settings.upgrades_speed_factor
 		
+		# Float values are converted to integers and assigned back to the rect.
 		self.rect.centerx = self.centerx
 		self.rect.centery = self.centery
 		
 	def blitme(self):
+		"""Draws the Upgrade onto the screen."""
+		# Only for individual upgrades, sprites are preferred to use
+		# self.draw(self.screen) or u_laser.draw(screen)
 		self.screen.blit(self.image, self.rect)
 		
