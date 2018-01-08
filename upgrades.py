edeb9839056d34b38ac27a530f77b108041222f2
letsdie10 <sngby98@gmail.com>
2018-01-08 15:04:45 +0000

    Refactored Upgrades into just one class, default - railgun image

--- upgrades.py
+++ upgrades.py
@@ -1,6 +1,51 @@
 import pygame
 from pygame.sprite import Sprite
 
+class Upgrade(Sprite):
+	"""Represents the Upgrade drop that will be shot down."""
+	def __init__(self, ai_settings, screen):
+		"""Initializes the Upgrade settings."""
+		super().__init__()
+		self.ai_settings = ai_settings
+		self.screen = screen
+		
+		# Loads image and get image rect (Default image is railguns).
+		self.image = pygame.image.load("images/upgrades/railguns.bmp")
+		#self.image = pygame.image.load("images/upgrades/secondary.bmp")
+		#self.image = pygame.image.load("images/upgrades/missile.bmp")
+		#self.image = pygame.image.load("images/upgrades/laser.bmp")
+		self.rect = self.image.get_rect()
+		
+		# Positions the upgrade image anywhere first.
+		self.rect.x = 0
+		self.rect.y = 0
+		
+		# Float values to a new variable if not decimals will not be registered.
+		self.centerx = float(self.rect.centerx)
+		self.centery = float(self.rect.centery)
+		
+		# Changing rect_shift changes the position of the
+		# upgrades offset.
+		self.rect_shift = 10
+		self.upgrade_timer = False
+		
+	def update(self):
+		"""Updates the position of the Upgrade."""
+		# Upgrades will be dropped and move to the left of the screen,
+		# towards the ship.
+		if not self.upgrade_timer:
+			self.centerx -= self.ai_settings.upgrades_speed_factor
+		
+		# Float values are converted to integers and assigned back to the rect.
+		self.rect.centerx = self.centerx
+		self.rect.centery = self.centery
+		
+	def blitme(self):
+		"""Draws the Upgrade onto the screen."""
+		# Only for individual upgrades, sprites are preferred to use
+		# self.draw(self.screen) or u_upgrades.draw(screen)
+		self.screen.blit(self.image, self.rect)
+
 class UpgradeRailguns(Sprite):
 	"""Represents the UpgradeRailguns drop that can be shot down."""
 	def __init__(self, ai_settings, screen):
@@ -21,11 +66,17 @@ class UpgradeRailguns(Sprite):
 		self.centerx = float(self.rect.centerx)
 		self.centery = float(self.rect.centery)
 		
+		# Changing rect_shift changes the position of the
+		# upgrades offset.
+		self.rect_shift = 10
+		self.upgrade_timer = False
+		
 	def update(self):
 		"""Updates the position of the Upgrade."""
 		# UpgradeRailguns will be dropped and move to the left of the screen,
 		# towards the ship.
-		self.centerx -= self.ai_settings.upgrades_speed_factor
+		if not self.upgrade_timer:
+			self.centerx -= self.ai_settings.upgrades_speed_factor
 		
 		# Float values are converted to integers and assigned back to the rect.
 		self.rect.centerx = self.centerx
